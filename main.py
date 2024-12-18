a7f957280434b17d0f2ede7c92122a842f32f069
Layvu <zemskyalexey.writer@mail.ru>
2024-12-18 23:53:14 +0500

    fixed: put

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ from bs4 import BeautifulSoup
 from fastapi import FastAPI, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.future import select
-from sqlalchemy import text
+from sqlalchemy import Column, Integer, String, select, text
+from pydantic import BaseModel, Field
+from typing import Optional
 
 # Категория товаров для парсинга
 CATEGORY = "elki-elovye-vetki-girlyandy"
 
 # Интервал между парсингами в секундах
-INTERVAL = 10
+INTERVAL = 500
 
 # Настройка логирования
 logging.basicConfig(level=logging.INFO)
@@ -38,7 +38,7 @@ async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession
 app = FastAPI()
 
 
-# Ассинхронный парсинг данных с сайта
+# Асинхронный парсинг данных с сайта
 async def fetch_product_data(url):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
@@ -82,10 +82,6 @@ async def fetch_product_data(url):
                 logger.error(f"Ошибка при обработке {url}: {str(e)}")
                 break
 
-    # Лог первых 3-х товаров для проверки
-    for i, product in enumerate(product_data[:3]):
-        logger.info(f"Product {i+1}: Name = {product['name']}, Price = {product['price']}")
-
     logger.info(f"Получено {len(product_data)} товаров")
     return product_data
 
@@ -93,21 +89,21 @@ async def fetch_product_data(url):
 # Запись данных в БД после её очистки
 async def save_products_to_db(products):
     async with async_session() as session:
-        async with session.begin():
-            try:
-                # Удаление всех старых записей
-                await session.execute(text('DELETE FROM products'))
-                logger.info("Old products deleted from the database.")
+        try:
+            # Удаление всех старых записей
+            await session.execute(text('DELETE FROM products'))
+            logger.info("Старые товары удалены из БД")
 
-                # Вставка новых данных
-                for product_data in products:
-                    product = Product(name=product_data["name"], price=product_data["price"])
-                    session.add(product)
+            # Вставка новых данных
+            for product_data in products:
+                product = Product(name=product_data["name"], price=product_data["price"])
+                session.add(product)
 
-                # Лог общего количества сохранённых товаров
-                logger.info(f"Сохранено {len(products)} товаров в базе данных")
-            except Exception as e:
-                logger.error(f"Ошибка при сохранении данных: {str(e)}")
+            # Сохранение изменений в БД
+            await session.commit()
+            logger.info(f"Сохранено {len(products)} товаров в базе данных")
+        except Exception as e:
+            logger.error(f"Ошибка при сохранении данных: {str(e)}")
 
 
 # Асинхронный периодический парсинг и запись данных в БД
@@ -123,6 +119,16 @@ async def periodic_parsing(interval: int = INTERVAL):
         logger.info(f"Ожидание {interval} секунд...")
         await asyncio.sleep(interval)
 
+
+# Модели для фильтрации и обновления данных
+class SUpdateFilter(BaseModel):
+    product_id: int  # Фильтр для поиска товара по id
+
+class SProductUpdate(BaseModel):
+    name: Optional[str] = Field(None, description="Новое имя товара")
+    price: Optional[str] = Field(None, description="Новая цена товара")
+
+
 # Маршрут для получения всех товаров из БД
 @app.get("/products")
 async def get_products():
@@ -130,6 +136,7 @@ async def get_products():
         result = await session.execute(select(Product))
         products = result.scalars().all()
         return products
+
 
 # Маршрут для получения товара по id
 @app.get("/products/{product_id}")
@@ -141,22 +148,38 @@ async def get_product(product_id: int):
             raise HTTPException(status_code=404, detail="Товар не найден")
         return product
 
+
 # Маршрут для редактирования товара по id
 @app.put("/products/{product_id}")
-async def update_product(product_id: int, name: str = None, price: str = None):
+async def update_product(product_id: int, new_data: SProductUpdate):
     async with async_session() as session:
+        # Получение товара из базы
         result = await session.execute(select(Product).filter(Product.id == product_id))
         product = result.scalar_one_or_none()
+
+        # Если товар не найден, возвращаем ошибку
         if product is None:
             raise HTTPException(status_code=404, detail="Товар не найден")
 
-        if name:
-            product.name = name
-        if price:
-            product.price = price
+        # Обновление данных товара
+        updated = False
 
-        await session.commit()
+        # Обновление имени товара, если передано новое
+        if new_data.name and new_data.name != product.name:
+            product.name = new_data.name
+            updated = True
+
+        # Обновление цены товара, если передано новое
+        if new_data.price and new_data.price != product.price:
+            product.price = new_data.price
+            updated = True
+
+        # Если обновления были, сохраняем изменения в БД
+        if updated:
+            await session.commit()
+
         return product
+
 
 # Маршрут для удаления товара по id
 @app.delete("/products/{product_id}")
@@ -169,10 +192,11 @@ async def delete_product(product_id: int):
 
         await session.delete(product)
         await session.commit()
-        return {"detail": "Product deleted"}
+        return {"detail": "Товар удалён"}
+
 
 # Запуск парсера
 @app.on_event("startup")
 async def startup_event():
-    logger.info("Начало асинхронного периодического парсинга...")
+    logger.info("Начало периодического парсинга")
     asyncio.create_task(periodic_parsing(INTERVAL))
